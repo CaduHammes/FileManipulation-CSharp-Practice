32fdd47ff0e18c0c85429d134a8c0d0e54c1d9a3
Cadu Hammes <duduhammes@hotmail.com>
2021-11-18 13:47:54 -0300

    File manipulation practice

---
+++ ExercicioDeFixacaoManipulacaoDeArquivos/ExercicioDeFixacaoManipulacaoDeArquivos/Entities/Produtos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDeFixacaoManipulacaoDeArquivos.Entities
+{
+    class Produtos
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produtos()
+        {
+        }
+
+        public Produtos(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + "," + SomaDosProdutos().ToString("f2", CultureInfo.InvariantCulture);
+        }
+
+        public double SomaDosProdutos()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
---
+++ ExercicioDeFixacaoManipulacaoDeArquivos/ExercicioDeFixacaoManipulacaoDeArquivos/Program.cs
@@ -0,0 +1,46 @@
+﻿using ExercicioDeFixacaoManipulacaoDeArquivos.Entities;
+using System;
+using System.IO;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace ExercicioDeFixacaoManipulacaoDeArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\duduh\AppData\Local\Temp";
+            string targetPath = @"C:\Users\duduh\AppData\Local\Temp\out\summary.csv";
+
+            try
+            {
+                Directory.CreateDirectory(sourcePath + "\\out");
+                string[] lines = File.ReadAllLines(sourcePath + "\\produtos.csv");
+                List<Produtos> produtos = new List<Produtos>();
+
+                using (StreamReader sr = File.OpenText(sourcePath + "\\produtos.csv"))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] valores = line.Split(",");
+                        Produtos produto = new Produtos(valores[0], double.Parse(valores[1], CultureInfo.InvariantCulture), int.Parse(valores[2]));
+                        produtos.Add(produto);
+                    }
+
+                    using (StreamWriter sw = File.AppendText(targetPath))
+                    {
+                        foreach (Produtos produto in produtos)
+                        {
+                            sw.WriteLine(produto);
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred: " + e.Message);
+            }
+        }
+    }
+}
